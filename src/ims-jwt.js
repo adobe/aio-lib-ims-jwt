39cc4d657a1062ce1d0f98b283edf14ebcd55ca0
Shazron Abdullah (shaz) <shazron@gmail.com>
2019-12-10 10:50:08 +0800

    fix: linting issues, add eslint shared config

--- src/ims-jwt.js
+++ src/ims-jwt.js
@@ -10,40 +10,39 @@ OF ANY KIND, either express or implied. See the License for the specific languag
 governing permissions and limitations under the License.
 */
 
-const jwt = require('jsonwebtoken');
-const debug = require('debug')('@adobe/aio-lib-core-ims-jwt/login');
+const jwt = require('jsonwebtoken')
+const debug = require('debug')('@adobe/aio-lib-core-ims-jwt/login')
 
-// The ims-login hook for JWT is taking care of calling IMS
-// the function takes the
-
-function canSupportSync(configData) {
+function configMissingKeys (configData) {
   if (!configData) {
-    return false;
+    return false
   }
 
-  const missing_keys = [];
-  const required_keys = ['client_id', 'client_secret', 'techacct', 'meta_scopes', 'ims_org_id', 'private_key'];
+  const missingKeys = []
+  const requiredKeys = ['client_id', 'client_secret', 'techacct', 'meta_scopes', 'ims_org_id', 'private_key']
 
-  required_keys.forEach(key => {
+  requiredKeys.forEach(key => {
     if (!configData[key]) {
-      missing_keys.push(key)
+      missingKeys.push(key)
     }
   })
 
-  return missing_keys.length == 0;
+  return missingKeys
 }
 
-async function canSupport(configData) {
-  if (canSupportSync(configData)) {
-    return Promise.resolve(true);
+const canSupportSync = (configData) => configMissingKeys(configData).length === 0
+
+async function canSupport (configData) {
+  const missingKeys = configMissingKeys(configData)
+  if (missingKeys.length === 0) {
+    return Promise.resolve(true)
   } else {
-    // TODO: Indicate that this is not really an error but just a possibility
-    return Promise.reject(`JWT not supported due to some missing properties: ${missing_keys}`);
+    return Promise.reject(new Error(`JWT not supported due to some missing properties: ${missingKeys}`))
   }
 }
 
-async function createJwt(ims, clientId, imsOrg, techacct, metaScopes, privateKey, passphrase) {
-  // new mechism: only JWT properties are in the configuration:
+async function createJwt (ims, clientId, imsOrg, techacct, metaScopes, privateKey, passphrase) {
+  // new mechanism: only JWT properties are in the configuration:
   // configData: An object providing the properties required for JWT with properties:
   //     imsOrg:        The IMS Org ID of the customer
   //     techacct:      The Technical Account field of the integration
@@ -55,18 +54,18 @@ async function createJwt(ims, clientId, imsOrg, techacct, metaScopes, privateKey
 
   // Prepare a short lived JWT token to exchange for an access token
   const payload = {
-    exp: Math.round(Date.now() / 1000 + 300),  // 5 minutes expiry time
+    exp: Math.round(Date.now() / 1000 + 300), // 5 minutes expiry time
     iss: imsOrg,
     sub: techacct,
-    aud: ims.getApiUrl("/c/" + clientId)
+    aud: ims.getApiUrl('/c/' + clientId)
   }
 
   // configure the metascope for the JWT (only one supported for now)
   for (const metaScope of metaScopes) {
-    payload[ims.getApiUrl("/s/" + metaScope)] = true
+    payload[ims.getApiUrl('/s/' + metaScope)] = true
   }
 
-  let keyParam = (typeof(privateKey) === "string") ? privateKey : privateKey.join('\n');
+  let keyParam = (typeof (privateKey) === 'string') ? privateKey : privateKey.join('\n')
   if (passphrase) {
     keyParam = {
       key: privateKey,
@@ -76,17 +75,17 @@ async function createJwt(ims, clientId, imsOrg, techacct, metaScopes, privateKey
 
   let jwtToken
   try {
-    jwtToken = jwt.sign(payload, keyParam, { algorithm: 'RS256' }, null);
-    debug("Signed JWT token: %s", jwtToken);
-    return jwtToken;
+    jwtToken = jwt.sign(payload, keyParam, { algorithm: 'RS256' }, null)
+    debug('Signed JWT token: %s', jwtToken)
+    return jwtToken
   } catch (err) {
-    debug("JWT signing failed: %s", err.message);
-    debug(err.stack);
+    debug('JWT signing failed: %s', err.message)
+    debug(err.stack)
     throw new Error('A passphrase is needed for your private-key. Use the --passphrase flag to specify one.')
   }
 }
 
-async function imsLogin(ims, config) {
+async function imsLogin (ims, config) {
   return canSupport(config)
     .then(() => createJwt(ims, config.client_id, config.ims_org_id, config.techacct, config.meta_scopes, config.private_key))
     .then(jwtToken => ims.exchangeJwtToken(config.client_id, config.client_secret, jwtToken))
